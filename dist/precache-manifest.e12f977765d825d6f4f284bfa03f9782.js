9d2ea64dbeb8d808b726d37c5fd32b39325c4776
mcison21 <mcison21@gmail.com>
2022-01-29 02:35:45 -0500

    Adding /dist folder to code

---
+++ dist/precache-manifest.e12f977765d825d6f4f284bfa03f9782.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c042cfe5852f41dd36",
+    "url": "/css/app.efd96848.css"
+  },
+  {
+    "revision": "768b47689118bf6c957c51ad328a069f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57c042cfe5852f41dd36",
+    "url": "/js/app.6d3b19c0.js"
+  },
+  {
+    "revision": "fd1358d6f5f6f3998c24",
+    "url": "/js/chunk-vendors.d0074f43.js"
+  },
+  {
+    "revision": "97bd7243a9512f1c77afe88de326b477",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a2f7b4d6b2e9af15c7545734b62bf7e3",
+    "url": "/selected-projects/approval/approval-1.png"
+  },
+  {
+    "revision": "b6fe1b84524885f60e216ede6199698f",
+    "url": "/selected-projects/approval/approval-2.png"
+  },
+  {
+    "revision": "ddf1b3e641c89e735478f4fee4f0109e",
+    "url": "/selected-projects/approval/approval-3.png"
+  },
+  {
+    "revision": "8fcc479b5f48d769db47bbc849de1060",
+    "url": "/selected-projects/approval/approval-4.png"
+  },
+  {
+    "revision": "6a56d6638cd3fd8ac43aa7f37d2ea7a1",
+    "url": "/selected-projects/approval/approval-5.png"
+  },
+  {
+    "revision": "7136419cd78f20c8ed675af8e474c1e5",
+    "url": "/selected-projects/approval/approval-6.png"
+  },
+  {
+    "revision": "41d46fecae68c25d714bf3377d95ea13",
+    "url": "/selected-projects/approval/approval-7.png"
+  },
+  {
+    "revision": "7dde0f2bbf452e640cb8cc68bbf1cd25",
+    "url": "/selected-projects/approval/approval-8.png"
+  },
+  {
+    "revision": "48be1e1755d5b6ffa1916266e6883b4c",
+    "url": "/selected-projects/approval/approval-9.png"
+  },
+  {
+    "revision": "a5e829d04a3eb221091539f96b295469",
+    "url": "/selected-projects/events/events-1.png"
+  },
+  {
+    "revision": "5c9e9f4c9e1956651fa04bea38030d6d",
+    "url": "/selected-projects/events/events-2.png"
+  },
+  {
+    "revision": "1c1e621d198707d4a415943a6bb9b0b6",
+    "url": "/selected-projects/events/events-3.png"
+  },
+  {
+    "revision": "3f2150121713398d38d361dc932f5be7",
+    "url": "/selected-projects/events/events-4.png"
+  },
+  {
+    "revision": "b91f606feaffc806e190b769cd6c5bd1",
+    "url": "/selected-projects/housing/housing-1.png"
+  },
+  {
+    "revision": "14e3ce94f4d5ae08dc1412e19f605746",
+    "url": "/selected-projects/housing/housing-10.png"
+  },
+  {
+    "revision": "c538f57fe6fa5a383fc3c5a51e9ac3c6",
+    "url": "/selected-projects/housing/housing-11.png"
+  },
+  {
+    "revision": "7823da1923fd664a9563d026b08a9751",
+    "url": "/selected-projects/housing/housing-2.png"
+  },
+  {
+    "revision": "5b8c6aac4b59d0f6e8f71f83bc916ba2",
+    "url": "/selected-projects/housing/housing-3.png"
+  },
+  {
+    "revision": "130375d7997c0464cba307f1335585a5",
+    "url": "/selected-projects/housing/housing-4.png"
+  },
+  {
+    "revision": "cea1117a79ae20de12fe328666f53a52",
+    "url": "/selected-projects/housing/housing-5.png"
+  },
+  {
+    "revision": "ba552e02d6a02118fea22efe80b523c8",
+    "url": "/selected-projects/housing/housing-6.png"
+  },
+  {
+    "revision": "2ce56577c9e2ea0478b71944932bbca6",
+    "url": "/selected-projects/housing/housing-7.png"
+  },
+  {
+    "revision": "81d9bbab49a0106150ec7901b8826fb1",
+    "url": "/selected-projects/housing/housing-8.png"
+  },
+  {
+    "revision": "79cbbecd0e5afddfc1b0dd40063b3b8c",
+    "url": "/selected-projects/housing/housing-9.png"
+  },
+  {
+    "revision": "6ae923a07bf9923677314f16584967b4",
+    "url": "/selected-projects/jobs/jobs-1.png"
+  },
+  {
+    "revision": "4bcbb480a712e1e8c580272b2dad7b36",
+    "url": "/selected-projects/jobs/jobs-2.png"
+  },
+  {
+    "revision": "6d5f43e7896154145ccfd9401923b793",
+    "url": "/selected-projects/jobs/jobs-3.png"
+  },
+  {
+    "revision": "ed503cc987550642ac5e366fa6db528e",
+    "url": "/selected-projects/jobs/jobs-4.png"
+  },
+  {
+    "revision": "ea4006642be20836a9e2f1c526990872",
+    "url": "/selected-projects/jobs/jobs-5.png"
+  },
+  {
+    "revision": "8770ef0f55bd12254d1bee8af00a91d8",
+    "url": "/selected-projects/jobs/jobs-6.png"
+  },
+  {
+    "revision": "8ff02f855362f9b9c839327be8e205fc",
+    "url": "/selected-projects/jobs/jobs-7.png"
+  },
+  {
+    "revision": "78319027900f84892f9ec8ace7b3e4d3",
+    "url": "/selected-projects/jobs/jobs-8.png"
+  },
+  {
+    "revision": "f2e261873079a69cd68f720adc233339",
+    "url": "/selected-projects/password-utility/password-utility-1.png"
+  },
+  {
+    "revision": "1d5385fff109670e642aaf3ede8762b1",
+    "url": "/selected-projects/password-utility/password-utility-2.png"
+  },
+  {
+    "revision": "f3fb1e3ea50de139919df1197b5c37a7",
+    "url": "/selected-projects/password-utility/password-utility-3.png"
+  },
+  {
+    "revision": "830325800ff87d6f12e961e1de065dd4",
+    "url": "/selected-projects/permits/permits-1.png"
+  },
+  {
+    "revision": "5de93219871dcb8af008069065aed638",
+    "url": "/selected-projects/permits/permits-2.png"
+  },
+  {
+    "revision": "89d7aaea7f152e36c295ef2946922161",
+    "url": "/selected-projects/permits/permits-3.png"
+  },
+  {
+    "revision": "8e9d5c42eb45ef3cda27527600a84c87",
+    "url": "/selected-projects/report/report-1.png"
+  },
+  {
+    "revision": "7afc9bb8412ddd0eade6a863d5b8f3b1",
+    "url": "/selected-projects/report/report-2.png"
+  },
+  {
+    "revision": "791389bf0873678616df8411ed4aaaff",
+    "url": "/selected-projects/report/report-3.png"
+  },
+  {
+    "revision": "01f2e6fd616c19fe057f2de7f457331e",
+    "url": "/selected-projects/report/report-4.png"
+  },
+  {
+    "revision": "052dac6f5a79ae5b03604e5ecf9318e7",
+    "url": "/selected-projects/report/report-5.png"
+  },
+  {
+    "revision": "f7a06b71596842f40291e30c7029118b",
+    "url": "/selected-projects/report/report-6.png"
+  },
+  {
+    "revision": "8b32476a7bd466a9b3c7e949d0c569a2",
+    "url": "/selected-projects/report/report-7.png"
+  },
+  {
+    "revision": "a2342e56eb7131b3eefe916cba36000a",
+    "url": "/selected-projects/report/report-8.png"
+  },
+  {
+    "revision": "bae07950d8c3edc30a4df842b65e5c28",
+    "url": "/selected-projects/report/report-9.png"
+  },
+  {
+    "revision": "168089e8e95cdf94b5c642fb7175f5f2",
+    "url": "/selected-projects/student-application/student-application-1.png"
+  },
+  {
+    "revision": "c102a96f8347d9b9d4f06f2f01772a71",
+    "url": "/selected-projects/student-application/student-application-2.png"
+  },
+  {
+    "revision": "939a00deae3e56b3efb52fe864f49bb1",
+    "url": "/selected-projects/student-application/student-application-3.png"
+  },
+  {
+    "revision": "8e9a5bcf30256665ba72436be340fa9c",
+    "url": "/selected-projects/student-application/student-application-4.png"
+  },
+  {
+    "revision": "04403d9e86b26bce216f59c9f13d5468",
+    "url": "/selected-projects/student-application/student-application-5.png"
+  },
+  {
+    "revision": "9a466f3f358fcf91ac4ec058f3a2a67d",
+    "url": "/selected-projects/student-application/student-application-6.png"
+  },
+  {
+    "revision": "2cf58e8e30eb9b8b5ce5721617b62c54",
+    "url": "/selected-projects/student-application/student-application-7.png"
+  },
+  {
+    "revision": "4466ba227899ef6d9f35f3012cf30721",
+    "url": "/selected-projects/student-application/student-application-8.png"
+  },
+  {
+    "revision": "b79f9387f67f80cd9a75d7148870a287",
+    "url": "/selected-projects/student-application/student-application-9.png"
+  }
+]);
